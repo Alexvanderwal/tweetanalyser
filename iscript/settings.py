dc73a8ac4a945cfbee667dadf59abe0a694b9926
Alex <amvvanderwal@live.nl>
2016-12-01 02:18:11 +0100

    verandering plaats static files

--- iscript/settings.py
+++ iscript/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/iscript/../static/general',
+    os.path.join(BASE_DIR, "iscript/static"),
+    '/iscript/../iscript/static/general',
 ]
 
 # twitter keys
